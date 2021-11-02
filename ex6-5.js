e1911e32c8448f234938813d4e808af2aa99d93c
Mi4ulement <zm19274@josai.ac.jp>
2021-11-02 01:51:39 +0000

    message

--- ex6-5.js
+++ ex6-5.js
@@ -1,9 +1,7 @@
 var who65 = 'world'
-function saykita() {alert('who65, +が来た');}
-
-function sayhello65() {
-    alert('Hello, ' + who65 + '!');
+function sayhello65() {alert('Hello, ' + who65 + '!');
+}
 function taro65() {who65 = '太郎';}
-function taro65()alert(saykita)
-function hanako65() {who65 = '花子';}alert(saykita)
-function hanako65(){alert('saykita');}
+function hanako65() {who65 = '花子';}
+
+function saykita(){alert(who65 +'が来た!');}
